fbdbd50ead4f4929a87503c5f660c8a6290acf91
Atharv Dange <atharvdange618@gmail.com>
2025-04-18 00:26:06 +0530

    updated package.json for deployment

--- utils/captureScreenshot.js
+++ utils/captureScreenshot.js
@@ -1,21 +1,21 @@
-const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
 async function captureScreenshot(url) {
+  const executablePath = await chromium.executablePath;
+
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: chromium.args,
+    executablePath: executablePath || undefined,
+    headless: chromium.headless,
+    defaultViewport: { width: 1920, height: 1080 },
   });
 
   const page = await browser.newPage();
-
-  // Set the viewport to 1920×1080
-  await page.setViewport({ width: 1920, height: 1080 });
-
   await page.goto(url, { waitUntil: "networkidle2" });
-
   const buffer = await page.screenshot({ fullPage: false });
 
   await browser.close();
-
   return buffer;
 }
 
